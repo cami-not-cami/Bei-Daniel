28d95cb4f0c0b7c3409ea0b444382131599fe7e0
Iulian Torz <117668600+torzchi@users.noreply.github.com>
2025-11-15 22:47:14 +0200

    settings page and InvoiceNumber updater

--- Bei Daniel/View/Pages/SettingsOverview.xaml.cs
+++ Bei Daniel/View/Pages/SettingsOverview.xaml.cs
@@ -1,30 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Bei_Daniel.ViewModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bei_Daniel.View.Pages
 {
-    /// <summary>
-    /// Interaction logic for SettingsOverview.xaml
-    /// </summary>
     public partial class SettingsOverview : Page
     {
         public SettingsOverview()
         {
             InitializeComponent();
-            DataContext = new ViewModel.SettingsViewModel();
-
+            DataContext = new SettingsViewModel();
         }
     }
 }
--- Bei Daniel/ViewModel/SettingsViewModel.cs
+++ Bei Daniel/ViewModel/SettingsViewModel.cs
@@ -1,34 +1,70 @@
 ﻿using Bei_Daniel.Helpers;
+using DinnerBoxd.Helpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Bei_Daniel.ViewModel
 {
     internal class SettingsViewModel : BaseClass
     {
-        public ICommand SetInvoiceStartNumber { get; set; }
-        public SettingsViewModel()
+        private string _invoiceNumberInput;
+        public string InvoiceNumberInput
         {
-            
+            get => _invoiceNumberInput;
+            set
+            {
+                _invoiceNumberInput = value;
+                OnPropertyChanged(nameof(InvoiceNumberInput));
+            }
+        }
+
+            public ICommand SetInvoiceStartNumber { get; set; }
+
+            public SettingsViewModel()
+            {
+                SetInvoiceStartNumber = new RelayCommand(ExecuteSetInvoiceStartNumber);
+                LoadCurrentInvoiceNumber();
+            }
+
+            private void LoadCurrentInvoiceNumber()
+        {
+            var folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+            var invoiceFile = Path.Combine(folder, "invoice_counter.txt");
+            if (File.Exists(invoiceFile))
+            {
+                InvoiceNumberInput = File.ReadAllText(invoiceFile).Trim();
+            }
+            else
+            {
+                InvoiceNumberInput = "1";
+            }
+        }
+
+        private void ExecuteSetInvoiceStartNumber()
+        {
+            if (string.IsNullOrWhiteSpace(InvoiceNumberInput))
+            {
+                MessageBox.Show("Invoice number cannot be empty.", "Validation", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            SetInvoiceStartNumberExecute(InvoiceNumberInput);
+            MessageBox.Show("Invoice number updated successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         public void SetInvoiceStartNumberExecute(string invoiceNumber)
         {
             var folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
             var invoiceFile = Path.Combine(folder, "invoice_counter.txt");
-            if (File.Exists(invoiceFile))
-            {
-                FileStream fs = new FileStream(invoiceFile, FileMode.Open, FileAccess.Write);
-                using (StreamWriter writer = new StreamWriter(fs))
-                {
-                    writer.Write(invoiceNumber);
-                }
-            }
+            
+            Directory.CreateDirectory(folder);
+            File.WriteAllText(invoiceFile, invoiceNumber);
         }
     }
 }
